614a0b4b4fe6eb7c8f921670bb1e7c63edfe2c7e
Brian Harney <brian.r.harney@gmail.com>
2021-06-24 14:56:34 -0500

    Add population geometries

--- web_map.py
+++ web_map.py
@@ -10,16 +10,34 @@ volcano_name = list(data["NAME"])
 elevation = list(data["ELEV"])
 
 
+def marker_color(elevation):
+    if elevation < 1500:
+        return "green"
+    elif elevation > 3000:
+        return "red"
+    else:
+        return "orange"
+
+
 fg = folium.FeatureGroup(name="My Map")
 
 for lat, lon, name, elev in zip(latitude, longitude, volcano_name, elevation):
     fg.add_child(
-        folium.Marker(
+        folium.CircleMarker(
             location=[lat, lon],
+            radius=6,
             popup=str(name) + ": Elevation: " + str(elev) + "m",
-            icon=folium.Icon(color="green"),
+            fill_color=marker_color(elev),
+            color="black",
+            fill_opacity=0.7,
         )
     )
+
+fg.add_child(
+    folium.GeoJson(data=(open("mapping/world.json", "r", encoding="utf-8-sig").read()))
+)
+
+
 map.add_child(fg)
 
 map.save("Map1.html")
