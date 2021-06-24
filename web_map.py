bcd42c857a1021b4cf3bb091f0b7dffc5c37a4cb
Brian Harney <brian.r.harney@gmail.com>
2021-06-24 14:33:47 -0500

    Generate markers using volcano data

--- web_map.py
+++ web_map.py
@@ -1,14 +1,22 @@
 import folium
+import pandas
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles=("Stamen Terrain"))
 
+data = pandas.read_csv("mapping/Volcanoes.txt")
+latitude = list(data["LAT"])
+longitude = list(data["LON"])
+volcano_name = list(data["NAME"])
+elevation = list(data["ELEV"])
+
+
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+for lat, lon, name, elev in zip(latitude, longitude, volcano_name, elevation):
     fg.add_child(
         folium.Marker(
-            location=coordinates,
-            popup="Hi I am a marker",
+            location=[lat, lon],
+            popup=str(name) + ": Elevation: " + str(elev) + "m",
             icon=folium.Icon(color="green"),
         )
     )
